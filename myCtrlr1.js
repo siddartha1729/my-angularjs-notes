9b2d1956c7b2d996aaab90412be9bc90ee71ca40
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2017-07-22 03:46:49 +0530

    updated code

--- myCtrlr1.js
+++ myCtrlr1.js
@@ -76,12 +76,12 @@ app
 
       let options = {
         method: 'GET' // 'POST';
-        , url: 'https://wdnrvm61mi.execute-api.us-west-2.amazonaws.com/dev/samplee/servicee1'
+        , url: 'https://abc1231mi.execute-api.us-west-2.amazonaws.com/dev/samplee/servicee1'
         , headers: {
           'Content-Type': 'application/json'
-          , "Authorization": 'eyJraWQiOiJtV2NzRGdzS1dhXC95TTE1YlRJV1F1Nm1cL3hzcllxa0xcL3BnXC9hQ0RjUnBHOD0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI4ZGE0YjFmMi1hYTk4LTQzZjQtOTg5Yi1jMWM2ODQ1NTc5MjgiLCJhdWQiOiI2b2dpbzRkZ29wdjExOGcxazhtZTRtZzVsciIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTQ5ODIxMTgyNiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLXdlc3QtMi5hbWF6b25hd3MuY29tXC91cy13ZXN0LTJfNXJ0Z1czQ1lGIiwiY29nbml0bzp1c2VybmFtZSI6InRlbGVtZXRyeXVzZXIxIiwiZXhwIjoxNDk4MjE1NDI2LCJpYXQiOjE0OTgyMTE4MjYsImVtYWlsIjoiVGVhbS1HSVMtQW1hem9uV2ViU2VydmljZXNBZG1pbnNAY2EuY29tIn0.PwR7Lo1WZXABpMbQ8XcninSUwxTO_uqLO3gCcNkY1_H2iaz0__KMbI7Hj-xbE9focE6DKbc4YdLlGqGYmFO-6MRfy4EbJE_0G0amkNBkVNCVy0LMfpPXtGd-viT-HUWNpw-Y5GF2pHfkMm4fIibZUzlL7aCG2pzzC4WS5hzHCxK5-rr78t3vdFTUNHjG6LjeM5vlrStcC2x1iaLVmA3UkZc8jSoZTUvLbaGPESWTfMlBsFOunSdCSEoQi4BASwZ3GH3jRgmbxb1iEBbbrH54i-cRvRfMwRWHl8JTjnPzlQm_6gClNBcAAoYlgMITBdLyPKhMAMPhOCO8oKZpSjpiYQ'
+          , "Authorization": 'bE9focE6DKbc4YdLlGqGYmF'
         }
-        , data: {"userId": "peey01"}
+        , data: {"userId": "y01"}
       };
 
       return callApi1($scope, $http, options);
